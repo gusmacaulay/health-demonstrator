edfba002cb556d870c9b3ae3dde14d4c4feea10e
Gus MacAulay <angus.macaulay@unimelb.edu.au>
2013-01-31 10:38:07 +1100

    Setup basic web app with extjs UI from Amir.

---
+++ src/main/webapp/init.js
@@ -0,0 +1,163 @@
+Ext.namespace('org.aurin.demonstratortools.health','org.aurin.demonstratortools.health.constants',
+'org.aurin.demonstratortools.health.constants.operator','org.aurin.demonstratortools.health.model',
+'org.aurin.demonstratortools.health.constants.UIOutputModel');
+org.aurin.demonstratortools.health.init=function(){
+//constants
+org.aurin.demonstratortools.health.constants.operator.LESS_THAN="LESS_THAN";
+org.aurin.demonstratortools.health.constants.operator.EQUAL_OR_LESS_THAN="EQUAL_OR_LESS_THAN";
+org.aurin.demonstratortools.health.constants.operator.EQUAL_OR_GREATER_THAN="EQUAL_OR_GREATER_THAN";
+org.aurin.demonstratortools.health.constants.operator.GREATER_THAN="GREATER_THAN";
+org.aurin.demonstratortools.health.constants.operator.EQUAL="EQUAL";
+//
+org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME="METRIC_NAME";
+org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION="METRIC_INCLUSION";
+org.aurin.demonstratortools.health.constants.UIOutputModel.OPERATOR="OPERATOR";
+org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_VALUE="METRIC_VALUE";
+
+org.aurin.demonstratortools.health.constants.UIOutputModel.SEIFA_METRIC="SEIFA_METRIC";
+org.aurin.demonstratortools.health.constants.UIOutputModel.TYPE2_DIABETES="TYPE2_DIABETES";
+org.aurin.demonstratortools.health.constants.UIOutputModel.DEPRESSION="DEPRESSION";
+org.aurin.demonstratortools.health.constants.UIOutputModel.OBESITY="OBESITY";
+org.aurin.demonstratortools.health.constants.UIOutputModel.SMOKING="SMOKING";
+org.aurin.demonstratortools.health.constants.UIOutputModel.NO_ACCESS_TO_GENERAL_PRACTICE="NO_ACCESS_TO_GENERAL_PRACTICE";
+org.aurin.demonstratortools.health.constants.UIOutputModel.BULK_BILLING_AND_FEE_BASED_SERVICE="BULK_BILLING_AND_FEE_BASED_SERVICE";
+org.aurin.demonstratortools.health.constants.UIOutputModel.BULK_BILLING_ONLY="BULK_BILLING_ONLY";
+org.aurin.demonstratortools.health.constants.UIOutputModel.FEE_ONLY="FEE_ONLY";
+org.aurin.demonstratortools.health.constants.UIOutputModel.AFTER_5_UP_UNTIL_8_PM_ON_WEEKDAYS="AFTER_5_UP_UNTIL_8_PM_ON_WEEKDAYS";
+org.aurin.demonstratortools.health.constants.UIOutputModel.AFTER_8_PM_ON_WEEKDAYS="AFTER_8_PM_ON_WEEKDAYS";
+org.aurin.demonstratortools.health.constants.UIOutputModel.ANY_SATURDAY_SERVICE_AFTER_12_NOON="ANY_SATURDAY_SERVICE_AFTER_12_NOON";
+org.aurin.demonstratortools.health.constants.UIOutputModel.ANY_SUNDAY_SERVICE="ANY_SUNDAY_SERVICE";
+org.aurin.demonstratortools.health.constants.UIOutputModel.COMMUNITY_HEALTH_CENTRE="COMMUNITY_HEALTH_CENTRE";
+org.aurin.demonstratortools.health.constants.UIOutputModel.MENTAL_HEALTH_SERVICE_PROVIDER="MENTAL_HEALTH_SERVICE_PROVIDER";
+
+//initializaiton
+org.aurin.demonstratortools.health.model.operator_store =[
+	[org.aurin.demonstratortools.health.constants.operator.LESS_THAN,"<"],
+	[org.aurin.demonstratortools.health.constants.operator.EQUAL_OR_LESS_THAN,"<="],
+	[org.aurin.demonstratortools.health.constants.operator.EQUAL_OR_GREATER_THAN,"=>"],
+	[org.aurin.demonstratortools.health.constants.operator.GREATER_THAN,">"],
+	[org.aurin.demonstratortools.health.constants.operator.EQUAL,"="]
+];				
+
+window['generateURL']= function(_serviceName){
+	//:todo it needs to be configured based on server URL matching bahaviour
+	var _url="";
+	//_url= Ext.String.format('/agent-walkability/{0}', _serviceName);
+	_url= Ext.String.format('{0}', _serviceName);
+	return _url
+}	
+
+ 
+org.aurin.demonstratortools.health.prepareUIParameteres= function ( ){	
+	var _UIObject=[];
+	var _componentValue;
+	//SEIFA
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.SEIFA_METRIC;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox11').getValue();
+	if (Ext.getCmp('checkbox11').getValue()){
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.OPERATOR]= Ext.getCmp('combo11').getValue();
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_VALUE]= Ext.getCmp('slider_value11').getValue();
+	}
+	_UIObject.push(_componentValue);
+	
+	//TYPE 2 Diabetes
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.TYPE2_DIABETES;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox12').getValue();
+	if (Ext.getCmp('checkbox12').getValue()){
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.OPERATOR]= Ext.getCmp('combo12').getValue();
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_VALUE]= Ext.getCmp('slider_value12').getValue();
+	}
+	_UIObject.push(_componentValue);
+	 
+	 //Depression
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.DEPRESSION;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox13').getValue();
+	if (Ext.getCmp('checkbox13').getValue()){
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.OPERATOR]= Ext.getCmp('combo13').getValue();
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_VALUE]= Ext.getCmp('slider_value13').getValue();
+	}
+	_UIObject.push(_componentValue);
+	
+	 //Obesity
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.OBESITY;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox14').getValue();
+	if (Ext.getCmp('checkbox14').getValue()){
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.OPERATOR]= Ext.getCmp('combo14').getValue();
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_VALUE]= Ext.getCmp('slider_value14').getValue();
+	}
+	_UIObject.push(_componentValue);
+	
+	//Smoking
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.SMOKING;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox15').getValue();
+	if (Ext.getCmp('checkbox15').getValue()){
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.OPERATOR]= Ext.getCmp('combo15').getValue();
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_VALUE]= Ext.getCmp('slider_value15').getValue();
+	}
+	_UIObject.push(_componentValue);
+	
+	//No Access to General Practice
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.NO_ACCESS_TO_GENERAL_PRACTICE;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox21').getValue();
+	if (Ext.getCmp('checkbox21').getValue()){
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.OPERATOR]= Ext.getCmp('combo21').getValue();
+		_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_VALUE]= Ext.getCmp('slider_value21').getValue();
+	}
+	_UIObject.push(_componentValue);
+	 
+	//	
+	//Bulk billing and fee based service
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.BULK_BILLING_AND_FEE_BASED_SERVICE;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox22').getValue();	 
+	_UIObject.push(_componentValue);
+	//Bulk billing only
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.BULK_BILLING_ONLY;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox23').getValue();	 
+	_UIObject.push(_componentValue);
+	//Fee only
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.FEE_ONLY;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox24').getValue();	 
+	_UIObject.push(_componentValue);
+	//After 5 up until 8 PM on weekdays
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.AFTER_5_UP_UNTIL_8_PM_ON_WEEKDAYS;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox25').getValue();	 
+	_UIObject.push(_componentValue);
+	//After 8 PM on weekdays
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.AFTER_8_PM_ON_WEEKDAYS;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox26').getValue();	 
+	_UIObject.push(_componentValue);
+	//Any Saturday service after 12 noon
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.ANY_SATURDAY_SERVICE_AFTER_12_NOON;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox27').getValue();	 
+	_UIObject.push(_componentValue);
+	//Any Sunday service
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.ANY_SUNDAY_SERVICE;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox28').getValue();	 
+	_UIObject.push(_componentValue);
+	//Community Health Centre
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.COMMUNITY_HEALTH_CENTRE;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox29').getValue();	 
+	_UIObject.push(_componentValue);
+	//Mental Health Service Provider
+	_componentValue={};
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_NAME]= org.aurin.demonstratortools.health.constants.UIOutputModel.MENTAL_HEALTH_SERVICE_PROVIDER;
+	_componentValue[org.aurin.demonstratortools.health.constants.UIOutputModel.METRIC_INCLUSION]= Ext.getCmp('checkbox30').getValue();	 
+	_UIObject.push(_componentValue);
+	return _UIObject;
+
+}   		 
+}		
---
+++ src/main/webapp/main.js
@@ -0,0 +1,738 @@
+ 
+
+org.aurin.demonstratortools.health.mainUI=function(){
+ 						 
+		 
+ 
+leftpanel  = new Ext.Panel({
+	title: 'Health Indicators',
+	height: 'auto',
+	frame: true,
+	
+	items: new Ext.form.FormPanel({
+		width: 600,
+		bodyPadding: 7,
+		defaults: {
+			anchor: '100%',
+			flex: 1,
+			style: {
+				padding: '3px'
+			}
+		},
+		layout: {                      
+			type: 'table',
+			columns: 7
+		},
+		frame:false,
+		//layoutConfig: {columns:2},
+		items: [ 
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox11',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'SEIFA Index of Relative Sociao-Economic Disadvantage',
+			fieldLabel: 'text',			 
+			checked: true,
+			colspan: 7,
+			listeners : {		
+				change : {
+					fn : function(checkbox, checked){
+						Ext.getCmp('combo11').setDisabled(!checked);
+						Ext.getCmp('slider11').setDisabled(!checked);
+						Ext.getCmp('slider_value11').setDisabled(!checked);
+					},
+				}
+			}
+			
+		} 
+		,
+		{			 
+			xtype : "combo",  
+			id: 'combo11',
+			hideLabel: true,			
+			value : org.aurin.demonstratortools.health.constants.operator.LESS_THAN,  			 					
+			store: org.aurin.demonstratortools.health.model.operator_store	,		 
+			width: 40,
+			colspan:1
+		},
+		{
+			xtype : "textfield",
+			id: 'slider_value11',
+			readOnly:true,	
+			width: 40,		
+			margins: '10',			
+			colspan: 1
+		},
+		{
+			xtype:'slider',
+			id: 'slider11',
+			hideLabel: true,
+			useTips: true,
+			width: 214,
+			value:50,
+			increment: 10,
+			minValue: 0,
+			maxValue: 100,
+			anchor: '100%',
+			listeners :{
+				change : function(select,thumb,newval,oldval){	
+					Ext.getCmp("slider_value11").setValue(thumb);
+				}
+            },
+			colspan: 4
+		},
+		{
+			xtype: 'label',			 
+			id:'slider_maxvalue11',				 
+			colspan: 1
+		}
+		,/////
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox12',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Type 2 Diabetes Prevalence',
+			fieldLabel: 'text',
+			colspan: 5,
+			checked: true,
+			listeners : {		
+				change : {
+					fn : function(checkbox, checked){
+						Ext.getCmp('combo12').setDisabled(!checked);
+						Ext.getCmp('slider12').setDisabled(!checked);
+						Ext.getCmp('slider_value12').setDisabled(!checked);
+					},
+				}
+			}
+			
+		},
+		{
+			xtype: 'label',	 
+			text: 'Rate Per 100 	SLA Residents',
+			margins: '0 0 0 10',
+			colspan: 2
+		}
+		,
+		{			 
+			xtype : "combo",  
+			id: 'combo12',
+			hideLabel: true,			
+			value : org.aurin.demonstratortools.health.constants.operator.LESS_THAN,  
+			 					
+			store: org.aurin.demonstratortools.health.model.operator_store	,		 
+			width: 40,
+			colspan: 1,
+		},
+		{
+			xtype : "textfield",
+			id: 'slider_value12',
+			readOnly:true,	
+			width: 40,		 
+			colspan: 1
+		},
+		{
+			xtype:'slider',
+			id: 'slider12',
+			hideLabel: true,
+			useTips: true,
+			width: 214,
+			value:50,
+			increment: 10,
+			minValue: 0,
+			maxValue: 100,			 
+			listeners :{
+				change : function(select,thumb,newval,oldval){	
+					Ext.getCmp("slider_value12").setValue(thumb);
+				}
+            },
+			colspan: 4
+		},
+		{
+			xtype: 'label',			 
+			id:'slider_maxvalue12',				 
+			colspan: 1
+		}
+		,////
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox13',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Depression(Mood Problems)',
+			fieldLabel: 'text',
+			colspan: 5,
+			checked: true,
+			listeners : {		
+				change : {
+					fn : function(checkbox, checked){
+						Ext.getCmp('combo13').setDisabled(!checked);
+						Ext.getCmp('slider13').setDisabled(!checked);
+						Ext.getCmp('slider_value13').setDisabled(!checked);
+					},
+				}
+			}
+			
+		},
+		{
+			xtype: 'label',
+			
+			text: 'Rate Per 100 	SLA Residents',
+			margins: '0 0 0 10',
+			colspan: 2
+		}
+		,
+		{			 
+			xtype : "combo",  
+			id: 'combo13',
+			hideLabel: true,			
+			value : org.aurin.demonstratortools.health.constants.operator.LESS_THAN,  
+			 					
+			store: org.aurin.demonstratortools.health.model.operator_store	,		 
+			width: 40,
+			colspan: 1
+		},
+		{
+			xtype : "textfield",
+			id: 'slider_value13',
+			readOnly:true,	
+			width: 40,		 
+			colspan: 1
+		},
+		{
+			xtype:'slider',
+			id: 'slider13',
+			hideLabel: true,
+			useTips: true,
+			width: 214,
+			value:50,
+			increment: 10,
+			minValue: 0,
+			maxValue: 100,
+			listeners :{
+				change : function(select,thumb,newval,oldval){	
+					Ext.getCmp("slider_value13").setValue(thumb);
+				}
+            },
+			colspan: 4
+		},
+		{
+			xtype: 'label',			 
+			id:'slider_maxvalue13',				 
+			colspan: 1
+		}
+		,/////
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox14',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Obesity',
+			fieldLabel: 'text',
+			colspan: 5,
+			checked: false,
+			listeners : {		
+				change : {
+					fn : function(checkbox, checked){
+						Ext.getCmp('combo14').setDisabled(!checked);
+						Ext.getCmp('slider14').setDisabled(!checked);
+						Ext.getCmp('slider_value14').setDisabled(!checked);
+					},
+				}
+			}
+			
+		},
+		{
+			xtype: 'label',
+			
+			text: 'Rate Per 100 	SLA Residents',
+			margins: '0 0 0 10',
+			colspan: 2
+		}
+		,
+		{			 
+			xtype : "combo",  
+			id: 'combo14',
+			hideLabel: true,			
+			value : org.aurin.demonstratortools.health.constants.operator.LESS_THAN,  
+			 					
+			store: org.aurin.demonstratortools.health.model.operator_store	,		 
+			width: 40,
+			disabled: true,
+			colspan: 1
+		},
+		{
+			xtype : "textfield",
+			id: 'slider_value14',
+			readOnly:true,	
+			width: 40,	
+			disabled: true,			
+			colspan: 1
+		},
+		{
+			xtype:'slider',
+			id: 'slider14',
+			hideLabel: true,
+			useTips: true,
+			width: 214,
+			value:50,
+			increment: 10,
+			minValue: 0,
+			maxValue: 100,
+			disabled: true,
+			listeners :{
+				change : function(select,thumb,newval,oldval){	
+					Ext.getCmp("slider_value14").setValue(thumb);
+				}
+            },
+			colspan: 4
+		},
+		{
+			xtype: 'label',			 
+			id:'slider_maxvalue14',				 
+			colspan: 1
+		}
+		,/////
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox15',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Smoking',
+			fieldLabel: 'text',
+			colspan: 5,
+			checked: false,
+			listeners : {		
+				change : {
+					fn : function(checkbox, checked){
+						Ext.getCmp('combo15').setDisabled(!checked);
+						Ext.getCmp('slider15').setDisabled(!checked);
+						Ext.getCmp('slider_value15').setDisabled(!checked);
+					},
+				}
+			}
+			
+		},
+		{
+			xtype: 'label',
+			
+			text: 'Rate Per 100 	SLA Residents',
+			margins: '0 0 0 10',
+			colspan: 2
+		}
+		,
+		{			 
+			xtype : "combo",  
+			id: 'combo15',
+			hideLabel: true,			
+			value : org.aurin.demonstratortools.health.constants.operator.LESS_THAN,  
+			 					
+			store: org.aurin.demonstratortools.health.model.operator_store	,		 
+			width: 40,
+			disabled: true,
+			colspan: 1
+		},
+		{
+			xtype : "textfield",
+			id: 'slider_value15',
+			readOnly:true,	
+			width: 40,	
+			disabled: true,			
+			colspan: 1
+		},
+		{
+			xtype:'slider',
+			id: 'slider15',
+			hideLabel: true,
+			useTips: true,
+			width: 214,
+			value:50,
+			increment: 10,
+			minValue: 0,
+			maxValue: 100,
+			disabled: true,
+			listeners :{
+				change : function(select,thumb,newval,oldval){	
+					Ext.getCmp("slider_value15").setValue(thumb);
+				}
+            },
+			colspan: 4
+		},
+		{
+			xtype: 'label',			 
+			id:'slider_maxvalue15',				 
+			colspan: 1
+		}
+		
+		]
+	})
+});
+	
+
+rightpanel  = new Ext.Panel({
+	title: 'Health Services',
+	height: 'auto',
+	 
+	frame: true,
+	items: new Ext.form.FormPanel({
+		width: 600,
+		bodyPadding: 7,
+		defaults: {
+			anchor: '100%',
+			flex: 1,
+			style: {
+				padding: '3px'
+			}
+		},
+		layout: {                      
+			type: 'table',
+			columns: 7
+		},
+		frame:false,
+		//layoutConfig: {columns:2},
+		items: [ 
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox21',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'General Practice',
+			fieldLabel: 'text',
+			colspan: 7,
+			checked: false,
+			listeners : {		
+				change : {
+					fn : function(checkbox, checked){
+						Ext.getCmp('combo21').setDisabled(!checked);
+						Ext.getCmp('slider21').setDisabled(!checked);
+						Ext.getCmp('slider_value21').setDisabled(!checked);
+						
+					},
+				}
+			}
+			
+		},
+		{
+			xtype: 'label',			
+			text: 'Area with no access to GP within',
+			margins: '0 0 0 10',
+			colspan: 7
+		}
+		,
+		{			 
+			xtype : "combo",  
+			id: 'combo21',
+			hideLabel: true,			
+			value : org.aurin.demonstratortools.health.constants.operator.LESS_THAN,  
+			 					
+			store: org.aurin.demonstratortools.health.model.operator_store	,		 
+			width: 40,
+			disabled: true,
+			colspan: 1
+		},
+		{
+			xtype : "textfield",
+			id: 'slider_value21',
+			readOnly:true,	
+			width: 40,		
+			disabled: true,			
+			colspan: 1
+		},
+		{
+			xtype:'slider',
+			id: 'slider21',
+			hideLabel: true,
+			useTips: true,
+			width: 214,
+			value:50,
+			increment: 10,
+			minValue: 0,
+			maxValue: 100,
+			disabled: true,
+			listeners :{
+				change : function(select,thumb,newval,oldval){	
+					Ext.getCmp("slider_value21").setValue(thumb);
+				}
+            },
+			colspan: 3
+		},
+		{
+			xtype: 'label',			 
+			id:'slider_maxvalue21',				 
+			colspan: 1
+		},
+		{
+			xtype: 'label',			
+			text: 'Meter',
+			margins: '0 0 0 10',
+			colspan: 1
+		},
+		{
+			 xtype: 'component',
+			 fieldLabel: '&nbsp;',
+			 labelSeparator: ' ',  
+			 colspan: 7
+		}
+		 ,/////////////
+		 {
+			xtype: 'label',
+			
+			text: 'Which has:',
+			margins: '0 0 0 10',
+			colspan: 7
+		}
+		,
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox22',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Bulk billing and fee based service',
+			fieldLabel: 'text',
+			colspan: 7,
+			checked: false		 
+			
+		} ,
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox23',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Bulk billing only',
+			fieldLabel: 'text',
+			colspan: 7,
+			checked: false		 
+			
+		},
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox24',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Fee only',
+			fieldLabel: 'text',
+			colspan: 7,
+			checked: false		 
+			
+		},
+		{
+			 xtype: 'component',
+			 fieldLabel: '&nbsp;',
+			 labelSeparator: ' ',  
+			 colspan: 7
+		}
+		,////////////////////
+		
+		 {
+			xtype: 'label',
+			
+			text: 'After Hours Services:',
+			margins: '0 0 0 10',
+			colspan: 7
+		}
+		,
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox25',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'After 5 up until 8 PM on weekdays',
+			fieldLabel: 'text',
+			colspan: 4,
+			checked: false		 
+			
+		} ,
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox26',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'After 8 PM on weekdays',
+			fieldLabel: 'text',
+			colspan: 3,
+			checked: false		 
+			
+		},
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox27',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Any Saturday service after 12 noon',
+			fieldLabel: 'text',
+			colspan: 4,
+			checked: false		 
+			
+		}, 
+		{
+			xtype: 'checkbox', 
+			id: 'checkbox28',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Any Sunday service',
+			fieldLabel: 'text',
+			colspan: 3,
+			checked: false		 
+			
+		}
+		,
+		{
+			 xtype: 'component',
+			 fieldLabel: '&nbsp;',
+			 labelSeparator: ' ',  
+			 colspan: 7
+		}
+		 ,////////////
+		 {
+			xtype: 'checkbox', 
+			id: 'checkbox29',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Community Health Centre',
+			fieldLabel: 'text',
+			colspan: 7,
+			checked: false		 
+			
+		},
+		{
+			 xtype: 'component',
+			 fieldLabel: '&nbsp;',
+			 labelSeparator: ' ',  
+			 colspan: 7
+		}
+		 ,////////////
+		 {
+			xtype: 'checkbox', 
+			id: 'checkbox30',
+			labelSeparator: '',
+			hideLabel: true,
+			boxLabel: 'Mental Health Service Provider',
+			fieldLabel: 'text',
+			colspan: 7,
+			checked: false		 
+			
+		}
+		]
+	})
+});
+org.aurin.demonstratortools.health.mainWindow = Ext.create('Ext.window.Window', {
+		id: 'recordWindow',
+		title: 'Health',
+		resizable: false,
+		closable: true,
+		width: '75%',
+		align:'center',
+		minWidth: 400,
+		//minHeight: 500,	 
+		layout: {                      
+			type: 'table',
+			columns: 2
+		},
+		plain:true,
+		modal: true,
+	 items: [
+		leftpanel ,rightpanel
+	],
+		buttons: [{
+			text: 'Run',
+			handler: function(w){
+				org.aurin.demonstratortools.health.run();
+			}
+		} ]
+	});
+ 
+org.aurin.demonstratortools.health.fetchMetricsConfiguration= function ( _callback ){	
+	 
+	var _url = 'fetchMetricsConfiguration.json';
+		Ext.Ajax.request({
+			url: _url,								 
+			success:function(data,options){	
+				console.log(data)
+				
+				if (data!= null &&data != undefined){
+					if (data.responseText!= null &&data.responseText != undefined){	
+							var _responseObj = Ext.decode(data.responseText);
+							Ext.getCmp('slider11').setMaxValue(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.SEIFA_METRIC]);
+							Ext.getCmp('slider_maxvalue11').setText(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.SEIFA_METRIC]);
+							
+							Ext.getCmp('slider12').setMaxValue(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.TYPE2_DIABETES]);
+							Ext.getCmp('slider_maxvalue12').setText(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.TYPE2_DIABETES]);
+							
+							Ext.getCmp('slider13').setMaxValue(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.DEPRESSION]);
+							Ext.getCmp('slider_maxvalue13').setText(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.DEPRESSION]);
+							
+							Ext.getCmp('slider14').setMaxValue(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.OBESITY]);
+							Ext.getCmp('slider_maxvalue14').setText(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.OBESITY]);
+							
+							Ext.getCmp('slider15').setMaxValue(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.SMOKING]);
+							Ext.getCmp('slider_maxvalue15').setText(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.SMOKING]);
+							
+							Ext.getCmp('slider21').setMaxValue(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.NO_ACCESS_TO_GENERAL_PRACTICE]);
+							Ext.getCmp('slider_maxvalue21').setText(_responseObj[org.aurin.demonstratortools.health.constants.UIOutputModel.NO_ACCESS_TO_GENERAL_PRACTICE]);
+							
+							_callback();						
+					}
+				}
+				 
+			},
+			failure:function(response,options){
+				new Ext.Window({
+								minHeight : '600px' ,
+								width: '50%',
+								id: 'messagewindowid',
+								//x: 0,
+								//y: 30,
+								layout: "fit",
+								html:response.responseText
+				}).show();	
+			}
+		});
+	} 
+org.aurin.demonstratortools.health.fetchMetricsConfiguration(function(){org.aurin.demonstratortools.health.mainWindow.show();});
+
+
+org.aurin.demonstratortools.health.run= function ( ){	
+	var _UIParams= org.aurin.demonstratortools.health.prepareUIParameteres();
+	var _encodedUIParams =  Ext.encode(_UIParams);
+	var _url = generateURL("run");
+		Ext.Ajax.request({
+			url: _url,		
+			params: {
+				'UIParams':_encodedUIParams,
+			},					 
+			success:function(data,options){	
+				console.log(data)
+				
+				if (data!= null &&data != undefined){
+					if (data.response!= null &&data.response != undefined){
+						if (data.response.record != null &&data.response.record != undefined){
+							//:todo 
+							
+							
+						}
+					}
+				}
+				 
+			},
+			failure:function(response,options){
+				new Ext.Window({
+								minHeight : '600px' ,
+								width: '50%',
+								id: 'messagewindowid',
+								//x: 0,
+								//y: 30,
+								layout: "fit",
+								html:response.responseText
+				}).show();	
+			}
+		});
+	} 
+ 
+}
+ 
+Ext.onReady(function(){
+	org.aurin.demonstratortools.health.init();
+	org.aurin.demonstratortools.health.mainUI();
+});
+
