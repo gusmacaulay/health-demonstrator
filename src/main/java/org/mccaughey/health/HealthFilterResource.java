a4990611bb7333094a0c1efcd6dd80850acc6b21
Gus Macaulay <amacaulay@50550WL-90925.(none)>
2013-01-31 15:01:40 +1100

    Added spring dependencies to pom, annotations for resource.

--- src/main/java/org/mccaughey/health/HealthFilterResource.java
+++ src/main/java/org/mccaughey/health/HealthFilterResource.java
@@ -1,5 +1,10 @@
 package org.mccaughey.health;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/health-filter")
 public class HealthFilterResource {
 
 }
