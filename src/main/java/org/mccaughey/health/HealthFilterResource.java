0510e0c2a010ea11b42ba1cfa5c4584b946f7907
Gus MacAulay <angus.macaulay@unimelb.edu.au>
2013-02-01 13:37:17 +1100

    Setup basic service and config stuff with spring-mvc.

--- src/main/java/org/mccaughey/health/HealthFilterResource.java
+++ src/main/java/org/mccaughey/health/HealthFilterResource.java
@@ -1,11 +1,21 @@
 package org.mccaughey.health;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/health-filter")
 public class HealthFilterResource {
-	
-
+  static final Logger LOGGER = LoggerFactory.getLogger(HealthFilterResource.class);
+  	
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public void getRegions(HttpServletResponse response, @PathVariable String id) throws Exception {
+    LOGGER.info("Got id:" + id);
+  }
 }
