516675b7d6d232df84574e16831d8161baa5780a
Gus MacAulay <angus.macaulay@unimelb.edu.au>
2013-02-07 11:24:09 +1100

    Added health filter test.

---
+++ src/test/java/org/mccaughey/health/HealthFilterTest.java
@@ -0,0 +1,13 @@
+package org.mccaughey.health;
+
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+public class HealthFilterTest extends TestCase {
+
+  public void testDoAnalyze() {
+    Map<String, Object> filterParameters;
+  }
+
+}
